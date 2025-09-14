23ee1514e1cf57f18aefdc326b08f9d7fb8ceb53
Lokeshr07 <lokeshlokeshr2005@gmail.com>
2025-09-15 00:11:50 +0530

    Add

--- src/DSA/LinkedLists/LinkedList.java
+++ src/DSA/LinkedLists/LinkedList.java
@@ -1,4 +1,30 @@
 package DSA.LinkedLists;
 
 public class LinkedList {
+    Node head;
+    public void insert(int data){
+        Node node = new Node(data);
+
+        if(head==null){
+            head = node;
+        }
+        else{
+            Node tail = head;
+            while(tail.next!=null){
+                tail=tail.next;
+            }
+
+            tail.next = node;
+        }
+    }
+
+    public void display(){
+        Node tail = head;
+        while(tail!=null){
+            System.out.print(tail.data+" ");
+            tail=tail.next;
+        }
+
+
+    }
 }
---
+++ src/DSA/LinkedLists/Main.java
@@ -0,0 +1,14 @@
+package DSA.LinkedLists;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+
+        list.display();
+    }
+}
