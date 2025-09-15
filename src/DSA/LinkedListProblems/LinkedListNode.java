eddb73a268114c7ce84191d361cd332a9f5a9c93
Lokeshr07 <lokeshlokeshr2005@gmail.com>
2025-09-15 23:58:12 +0530

    Linked List 1

---
+++ src/DSA/LinkedListProblems/LinkedListNode.java
@@ -0,0 +1,51 @@
+package DSA.LinkedListProblems;
+
+public class LinkedListNode {
+    ListNode head;
+
+    public void insert(int data)
+    {
+        ListNode newNode = new ListNode(data);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            ListNode tail = head;
+
+            while(tail!=null){
+                tail = tail.next;
+            }
+            tail = newNode;
+        }
+    }
+
+    public void display(){
+        ListNode tail = head;
+
+        while(tail!=null)
+        {
+            System.out.print(tail.data +" ");
+            tail = tail.next ;
+        }
+
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0,head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for(int i=0;i<=n;i++){
+            fast = fast.next;
+        }
+
+        while(fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
---
+++ src/DSA/LinkedListProblems/Remove_Nth_NodeFromEndofList.java
@@ -0,0 +1,29 @@
+package DSA.LinkedListProblems;
+
+//Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+//Input: head = [1,2,3,4,5], n = 2
+//Output: [1,2,3,5]
+//Example 2:
+//
+//Input: head = [1], n = 1
+//Output: []
+//Example 3:
+//
+//Input: head = [1,2], n = 1
+//Output: [1]
+public class Remove_Nth_NodeFromEndofList {
+
+   public static void main(String[] args){
+       LinkedListNode list =new LinkedListNode();
+       int arr[] = {1,2,3,4,5};
+       for(int i=0;i<arr.length;i++) {
+           list.insert(arr[i]);
+       }
+
+
+       ListNode n = list.removeNthFromEnd(list.head,2);
+       System.out.println(n.toString());
+   }
+
+}
