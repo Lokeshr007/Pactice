82af9aaddcf79dc60b6285df8c6bd8807de419a2
Lokeshr07 <lokeshlokeshr2005@gmail.com>
2025-09-20 19:07:21 +0530

    Stack Operations

---
+++ src/DSA/Stack/Runner.java
@@ -0,0 +1,15 @@
+package DSA.Stack;
+
+public class Runner {
+    public static void main(String[] args){
+        Stack nums = new Stack();
+        nums.push(1);
+        nums.push(2);
+        nums.push(3);
+        nums.push(4);
+        nums.push(5);
+        System.out.println(nums.pop());
+        System.out.println(nums.peek());
+        nums.show();
+    }
+}
