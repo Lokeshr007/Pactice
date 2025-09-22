10085e59167f513f0ced6147669a2225c31d44b7
Lokeshr07 <lokeshlokeshr2005@gmail.com>
2025-09-22 23:51:41 +0530

    Queue Enqueue

--- src/DSA/Queue/Queue.java
+++ src/DSA/Queue/Queue.java
@@ -1,5 +1,23 @@
 package DSA.Queue;
 
+
+
 public class Queue {
+    int queue[] = new int[5];
+    int size;
+    int front;
+    int rear;
+
+    public void enQueue(int data){
+        queue[rear]=data;
+        rear = rear+1;
+        size +=1;
+    }
+    public void show(){
+        System.out.println("Elements ");
+        for(int i=0;i<size;i++){
+            System.out.print(queue[i]+" ");
+        }
+    }
 
 }
--- src/DSA/Queue/Runner.java
+++ src/DSA/Queue/Runner.java
@@ -2,4 +2,14 @@ package DSA.Queue;
 
 public class Runner {
 
+    public static void main(String[] args)
+    {
+        Queue q =new Queue();
+        q.enQueue(10);
+        q.enQueue(20);
+        q.enQueue(30);
+        q.enQueue(40);
+
+        q.show();
+    }
 }
